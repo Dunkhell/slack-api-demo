f70a75cbb4c926907e3fadbb9a9d644353e10853
Dunell <ciemnoki12@gmail.com>
2022-03-23 11:37:48 +0100

    Ready demo

---
+++ src/test/java/com/slack/api/bot/slackinhousebot/SlackInHouseBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slack.api.bot.slackinhousebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackInHouseBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
